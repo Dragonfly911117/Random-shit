3fbe69292301b13bbd1313820626ad77e1e802c5
Dragonfly911117 <55215882+Dragonfly911117@users.noreply.github.com>
2024-12-07 10:12:24 +0800

    Update scoop_update_checker.ps1

--- code/script/ps1/scoop_update_checker.ps1
+++ code/script/ps1/scoop_update_checker.ps1
@@ -12,12 +12,12 @@ Write-Host "Checking for updates..."
 
 try {
     # Get status of all apps and check if there's an update available
-    $status = scoop status | Out-String
-    if ($status -match "\[update\]") {
+    $status = scoop status | gawk '$2 != $3 {print $1}' | Out-String
+    if ($status -eq "") {
         # Pop a notification if updates are available
-        New-BurntToastNotification -Text "Scoop Updates Available", "There are updates available for Scoop and its applications. Run 'scoop update *' to update."
-    } else {
         New-BurntToastNotification -Text "All applications are up to date."
+    } else {
+        New-BurntToastNotification -Text "Scoop Updates Available", "There are updates available for Scoop and its applications. Run 'scoop update *' to update."
     }
 } catch {
     Write-Host "[ERROR] Failed to check for updates."
